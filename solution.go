83b0c00a3343345465f104fbff1335a799963bf9
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-02-18 02:41:25 +0600

    end

--- solution.go
+++ solution.go
@@ -10,7 +10,7 @@ const (
 
 func CalcSquare(sideLen float64, sidesNum int) float64 {
 	if sidesNum == SidesCircle {
-		return 2 * sideLen * math.Pi
+		return sideLen * math.Pi * math.Pi
 	}
 	if sidesNum == SidesSquare {
 		return sideLen * sideLen
