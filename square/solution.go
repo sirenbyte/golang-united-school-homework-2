f06961e98ead5001a5f973881ef00db578214786
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-02-18 02:01:28 +0600

    test

--- square/solution.go
+++ square/solution.go
@@ -1,16 +1,20 @@
 package square
 
-import "math"
+const SidesCircle = 0
+const SidesSquare = 4
+const SidesTriangle = 3
 
 func CalcSquare(sideLen float64, sidesNum string) float64 {
-	if sidesNum == "SidesCircle" {
+	/*if sidesNum == SidesCircle {
 		return 2 * sideLen * math.Pi
 	}
-	if sidesNum == "SidesSquare" {
+	if sidesNum == SidesSquare {
 		return sideLen * sideLen
 	}
-	if sidesNum == "SidesTriangle" {
+	if sidesNum == SidesTriangle {
 		return ((sideLen * sideLen) * math.Sqrt(3)) / 4
 	}
+	return 0*/
+
 	return 0
 }
