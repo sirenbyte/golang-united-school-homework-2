5c890e2f7b65a0c9006ab698b8d59b7c9788dc5f
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-02-18 02:45:38 +0600

    correct circle

--- solution.go
+++ solution.go
@@ -10,7 +10,7 @@ const (
 
 func CalcSquare(sideLen float64, sidesNum int) float64 {
 	if sidesNum == SidesCircle {
-		return sideLen * math.Pi * math.Pi
+		return sideLen * sideLen * math.Pi
 	}
 	if sidesNum == SidesSquare {
 		return sideLen * sideLen
