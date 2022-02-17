992fb08ceb9359428d0dd51557d5db61164211a3
Abzal Tuganbay <abzal.tugan@gmail.com>
2022-02-18 01:53:37 +0600

    three com

--- square/solution.go
+++ square/solution.go
@@ -2,22 +2,14 @@ package square
 
 import "math"
 
-type IntCustomType int
-
-const (
-	SidesTriangle = 3
-	SidesSquare   = 4
-	SidesCircle   = 0
-)
-
-func CalcSquare(sideLen float64, sidesNum IntCustomType) float64 {
-	if sidesNum == SidesCircle {
+func CalcSquare(sideLen float64, sidesNum string) float64 {
+	if sidesNum == "SidesCircle" {
 		return 2 * sideLen * math.Pi
 	}
-	if sidesNum == SidesSquare {
+	if sidesNum == "SidesSquare" {
 		return sideLen * sideLen
 	}
-	if sidesNum == SidesTriangle {
+	if sidesNum == "SidesTriangle" {
 		return ((sideLen * sideLen) * math.Sqrt(3)) / 4
 	}
 	return 0
